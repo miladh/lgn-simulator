a793dccb6575f4d1e8e931ba2cd302bb8ce031ee
Milad <miladh@fys.uio.no>
2015-09-28 16:20:43 +0200

    added new tests

--- src/stimuli/patchgrating.cpp
+++ src/stimuli/patchgrating.cpp
@@ -17,8 +17,8 @@ double PatchGrating::valueAtPoint(vec2 rVec, double t)
 {
     vec dr = rVec - vec{0.5, 0.5};
     double r = sqrt(dot(dr, dr));
-    double s = (1 - Functions::heaviside(r - m_spotDiameter * 0.5))
-            * m_contrast * cos(dot(m_k, dr) - m_w * t);
+    double s = m_contrast * (1 - Functions::heaviside(r - m_spotDiameter * 0.5))
+             * cos(dot(m_k, dr) - m_w * t);
 
     return s;
 
@@ -35,18 +35,3 @@ double PatchGrating::fourierTransformAtFrequency(vec2 kVec, double w)
     return s;
 
 }
-
-
-//double arg1 = sqrt(dot(kVec - m_k, kVec - m_k)) * m_spotDiameter * 0.5;
-//double arg2 = sqrt(dot(kVec + m_k, kVec + m_k)) * m_spotDiameter * 0.5;
-//double s1 = Functions::delta(w, m_w);
-//double s2 = Functions::delta(w, -m_w);
-
-//if(arg1 != 0 && s1!=0){
-//    s1 *= 2. * Functions::secondKindBesselFunction(arg1)/arg1;
-//}
-
-//if(arg2 != 0 && s2!=0){
-//    s2 *= 2. * Functions::secondKindBesselFunction(arg2)/arg2;
-//}
-//return (s1+s2)*0.5*m_contrast * PI * PI * m_spotDiameter * m_spotDiameter * 0.5;
--- tests/mathTests.cpp
+++ tests/mathTests.cpp
@@ -4,33 +4,49 @@
 SUITE(SPECIALFUNCTIONS){
 
 
-TEST(dog) {
+    TEST(secondKindBesselFunction) {
+        CHECK_EQUAL(Functions::secondKindBesselFunction(-1.2), 0);
+        CHECK_EQUAL(Functions::secondKindBesselFunction(0.0), 1.);
+        CHECK_EQUAL(Functions::secondKindBesselFunction(2.2), 1.);
+    }
 
 
-    DOG dog(1.0, 0.25, 0.85, 0.83);
-    CHECK_CLOSE(dog.real({0.5, 0.1}), -0.189791527743, 1e-12);
-    CHECK_CLOSE(dog.real({1.2, 1.9}), -0.00025733892027, 1e-12);
+    TEST(heaviside) {
+        CHECK_EQUAL(Functions::heaviside(-1.2), 0);
+        CHECK_EQUAL(Functions::heaviside(0.0), 1.);
+        CHECK_EQUAL(Functions::heaviside(2.2), 1.);
+    }
 
-    CHECK_CLOSE(dog.complex({0.5, 1.1}), 0.316423256919, 1e-12);
-    CHECK_CLOSE(dog.complex({1.5, 0.1}), 0.389361200098, 1e-12);
+    TEST(delta) {
+        CHECK_EQUAL(Functions::delta(0,0), 1);
+        CHECK_EQUAL(Functions::delta(-1e6,-1e6), 1);
+        CHECK_EQUAL(Functions::delta(3.456,3.456), 1);
+        CHECK_EQUAL(Functions::delta(2.1,-2.1), 0);
+        CHECK_EQUAL(Functions::delta(1.,3.), 0);
+        CHECK_EQUAL(Functions::delta(-1230.,-50.), 0);
+    }
 
-}
+    TEST(isOdd) {
+        CHECK_EQUAL(Functions::isOdd(0), 0);
+        CHECK_EQUAL(Functions::isOdd(-3),1);
+        CHECK_EQUAL(Functions::isOdd(2), 0);
+        CHECK_EQUAL(Functions::isOdd(2.9), 0);
+        CHECK_EQUAL(Functions::isOdd(-3.6),1);
+        CHECK_EQUAL(Functions::isOdd(91), 1);
+    }
+
+
+    TEST(dog) {
+        DOG dog(1.0, 0.25, 0.85, 0.83);
+        CHECK_CLOSE(dog.real({0.5, 0.1}), -0.189791527743, 1e-12);
+        CHECK_CLOSE(dog.real({1.2, 1.9}), -0.00025733892027, 1e-12);
+
+        CHECK_CLOSE(dog.complex({0.5, 1.1}), 0.316423256919, 1e-12);
+        CHECK_CLOSE(dog.complex({1.5, 0.1}), 0.389361200098, 1e-12);
+
+    }
 
 
 
-TEST(heaviside) {
-    CHECK_EQUAL(Functions::heaviside(-1.2), 0);
-    CHECK_EQUAL(Functions::heaviside(2.2), 1.);
-
-}
-
-//TEST(loopKernel) {
-//    Config cfg;
-//    cfg.readFile("../../eDOG/tests/configTests.cfg");
-//    ImpulseResponse G(&cfg);
-//    CHECK_CLOSE(G.loopKernel(0.1, 5.9), 0.00124325581819, 1e-12);
-//    CHECK_CLOSE(G.loopKernel(1.5, 0.1), 0.338789712712, 1e-12);
-
-//}
 
 }
