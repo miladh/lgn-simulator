26de6a4b9b397b5dafefab63d2df43ffcd3e638e
Milad <miladh@fys.uio.no>
2015-09-30 19:14:57 +0200

    updates

--- tools/Simulation.py
+++ tools/Simulation.py
@@ -6,11 +6,13 @@ class Simulation:
 
     """
     def __init__(self, h5_file):
-        self.num_cell_types =  len(h5_file.keys())
+        self.numCellTypes =  len(h5_file.keys())
+        self.nSpatialPoints = 256 #READ FROM FILE!!!!!
+        self.nTemporalSteps = 16  #READ FROM FILE!!!!!
 
         for item in h5_file.keys():
             if(item== "stimuli"):
-                self.num_cell_types -=1
+                self.numCellTypes -=1
                 spatioTemporal = np.array(h5_file.get("/" + str(item)+ "/real"))
                 fourierTransform = np.array(h5_file.get("/" + str(item)+ "/complex"))
                 data = {"spatioTemporal": spatioTemporal, "fourierTransform": fourierTransform}
@@ -27,12 +29,24 @@ class Simulation:
                 setattr(self, item, data)
 
 
+    def singleCellTemporalResponse(self, cellType, idx=0 , idy=0):
+        response = getattr(self, cellType)["response"]["spatioTemporal"][:,idx, idy]
+        return response
+
+    def singleCellFreqResponse(self, cellType, idx=0 , idy=0):
+        FreqResponse = getattr(self, cellType)["response"]["fourierTransform"][:,idx, idy]
+        return FreqResponse
+
+
 if __name__ == "__main__":
     import h5py
     from glob import glob
+    from pylab import*
     outputFilePath = "/home/milad/Dropbox/projects/edog/extendedDOG/eDOG/DATA/*.h5"
     outputFile = glob(outputFilePath)[0]
     f = h5py.File(outputFile, "r")
     sim = Simulation(f)
-    print sim.ganglion["response"]["spatioTemporal"]
+    print sim.ganglion["response"]["spatioTemporal"].shape
     print sim.stimuli.keys()
+    plot(sim.singleCellFreqResponse("ganglion"))
+    show()
