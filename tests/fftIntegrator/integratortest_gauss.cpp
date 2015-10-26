d407aaf544874840c44c54775525c1637b15bb50
Milad <miladh@fys.uio.no>
2015-10-26 12:45:40 +0100

    new tests...gauss

--- tests/fftIntegrator/integratortest_gauss.cpp
+++ tests/fftIntegrator/integratortest_gauss.cpp
@@ -68,7 +68,7 @@ SUITE(INTEGRATOR){
             for(int i = 0; i < Ns; i++){
                 for(int j = 0; j < Ns; j++){
                     CHECK_CLOSE(real(g(i,j,l)),
-                                real(G(i,j,l)), 1e-9);
+                                real(G(i,j,l)), 1e-12);
 
                 }
             }
@@ -77,10 +77,9 @@ SUITE(INTEGRATOR){
 
 
 
-    TEST(gaussSpatialCosineTemporal){
-        //Mesh
-        int ns = 7;
-        int nt = 4;
+    TEST(gauss_0){
+        int ns = 9;
+        int nt = 2;
         double ds = 0.1;
         double dt = 0.1;
 
@@ -94,35 +93,33 @@ SUITE(INTEGRATOR){
         vec t = integrator.timeVec();
         vec w = integrator.temporalFreqVec();
 
-        double a = 2.1;
-        double wd = w(w.n_elem/2+3);
-
         cx_cube g = zeros<cx_cube>(Ns, Ns, Nt);
         cx_cube G = zeros<cx_cube>(Ns, Ns, Nt);
         cx_cube f = zeros<cx_cube>(Ns, Ns, Nt);
 
+        double wd = w(2);
+        double a =0.1;
 
         //Spatiotemporal signal
         for(int l = 0; l < Nt; l++){
             for(int i = 0; i < Ns; i++){
                 for(int j = 0; j < Ns; j++){
-                    g(i,j,l) = gauss(a, {s[i], s[j]})
-                            * cos(wd * t[l]);
+                    g(i,j,l) =gauss(a, {s[i], s[j]}) * cos(wd * t[l]);
                 }
             }
         }
-
 
         //fourier signal
         for(int l = 0; l < Nt; l++){
             for(int i = 0; i < Ns; i++){
                 for(int j = 0; j < Ns; j++){
                     f(i,j,l) = gaussFT(a, {k[i], k[j]})
-                            * Functions::delta(wd, w[l])*2*PI;
+                            * Functions::delta(wd, w[l]);
                 }
             }
         }
 
+        f *= 2*PI;
         f /= integrator.temporalFreqResolution();
 
         // Backward
@@ -134,62 +131,188 @@ SUITE(INTEGRATOR){
             for(int i = 0; i < Ns; i++){
                 for(int j = 0; j < Ns; j++){
                     CHECK_CLOSE(real(g(i,j,l)),
-                                real(G(i,j,l)), 1e-9);
-
+                                real(G(i,j,l)), 1e-12);
                 }
             }
         }
     }
-
-
-
-    TEST(gaussSpatial){
-        //Mesh
-        int ns = 7;
+    TEST(gauss_1){
+        int ns = 8;
+        int nt = 2;
         double ds = 0.1;
-        double a = 2.1;
-
-        Integrator integrator(0, 0, ns, ds);
+        double dt = 0.1;
 
         int Ns = pow(2,ns);
-        cx_mat g = zeros<cx_mat>(Ns, Ns);
-        cx_mat G = zeros<cx_mat>(Ns, Ns);
-        cx_mat f = zeros<cx_mat>(Ns, Ns);
+        int Nt = pow(2,nt);
+
+        Integrator integrator(nt, dt, ns, ds);
 
         vec s = integrator.coordinateVec();
         vec k = integrator.spatialFreqVec();
+        vec t = integrator.timeVec();
+        vec w = integrator.temporalFreqVec();
 
+        cx_cube g = zeros<cx_cube>(Ns, Ns, Nt);
+        cx_cube G = zeros<cx_cube>(Ns, Ns, Nt);
+        cx_cube f = zeros<cx_cube>(Ns, Ns, Nt);
 
-        //signal
-        for(int i = 0; i < Ns; i++){
-            for(int j = 0; j < Ns; j++){
-                g(i,j) = gauss(a, {s[i], s[j]});
+        double wd = w(2);
+        double a =2.2;
+
+        //Spatiotemporal signal
+        for(int l = 0; l < Nt; l++){
+            for(int i = 0; i < Ns; i++){
+                for(int j = 0; j < Ns; j++){
+                    g(i,j,l) =gauss(a, {s[i], s[j]}) * cos(wd * t[l]);
+                }
             }
         }
-
 
         //fourier signal
-        for(int i = 0; i < Ns; i++){
-            for(int j = 0; j < Ns; j++){
-                f(i,j) = gaussFT(a, {k[i], k[j]});
+        for(int l = 0; l < Nt; l++){
+            for(int i = 0; i < Ns; i++){
+                for(int j = 0; j < Ns; j++){
+                    f(i,j,l) = gaussFT(a, {k[i], k[j]})
+                            * Functions::delta(wd, w[l]);
+                }
             }
         }
+
+        f *= 2*PI;
+        f /= integrator.temporalFreqResolution();
 
         // Backward
         G = integrator.integrate(f);
         G = FFTHelper::fftShift(G);
 
-
-        //Test
-        for(int i = 0; i < Ns; i++){
-            for(int j = 0; j < Ns; j++){
-                CHECK_CLOSE(real(g(i,j)),
-                            real(G(i,j)), 1e-9);
-
+        // Test
+        for(int l = 0; l < Nt; l++){
+            for(int i = 0; i < Ns; i++){
+                for(int j = 0; j < Ns; j++){
+                    CHECK_CLOSE(real(g(i,j,l)),
+                                real(G(i,j,l)), 1e-12);
+                }
             }
         }
     }
+    TEST(gauss_2){
+        int ns = 8;
+        int nt = 2;
+        double ds = 0.1;
+        double dt = 0.1;
 
+        int Ns = pow(2,ns);
+        int Nt = pow(2,nt);
+
+        Integrator integrator(nt, dt, ns, ds);
+
+        vec s = integrator.coordinateVec();
+        vec k = integrator.spatialFreqVec();
+        vec t = integrator.timeVec();
+        vec w = integrator.temporalFreqVec();
+
+        cx_cube g = zeros<cx_cube>(Ns, Ns, Nt);
+        cx_cube G = zeros<cx_cube>(Ns, Ns, Nt);
+        cx_cube f = zeros<cx_cube>(Ns, Ns, Nt);
+
+        double wd = w(2);
+        double a =5.8;
+
+        //Spatiotemporal signal
+        for(int l = 0; l < Nt; l++){
+            for(int i = 0; i < Ns; i++){
+                for(int j = 0; j < Ns; j++){
+                    g(i,j,l) =gauss(a, {s[i], s[j]}) * cos(wd * t[l]);
+                }
+            }
+        }
+
+        //fourier signal
+        for(int l = 0; l < Nt; l++){
+            for(int i = 0; i < Ns; i++){
+                for(int j = 0; j < Ns; j++){
+                    f(i,j,l) = gaussFT(a, {k[i], k[j]})
+                            * Functions::delta(wd, w[l]);
+                }
+            }
+        }
+
+        f *= 2*PI;
+        f /= integrator.temporalFreqResolution();
+
+        // Backward
+        G = integrator.integrate(f);
+        G = FFTHelper::fftShift(G);
+
+        // Test
+        for(int l = 0; l < Nt; l++){
+            for(int i = 0; i < Ns; i++){
+                for(int j = 0; j < Ns; j++){
+                    CHECK_CLOSE(real(g(i,j,l)),
+                                real(G(i,j,l)), 1e-12);
+                }
+            }
+        }
+    }
+    TEST(gauss_3){
+        int ns = 8;
+        int nt = 2;
+        double ds = 0.01;
+        double dt = 0.1;
+
+        int Ns = pow(2,ns);
+        int Nt = pow(2,nt);
+
+        Integrator integrator(nt, dt, ns, ds);
+
+        vec s = integrator.coordinateVec();
+        vec k = integrator.spatialFreqVec();
+        vec t = integrator.timeVec();
+        vec w = integrator.temporalFreqVec();
+
+        cx_cube g = zeros<cx_cube>(Ns, Ns, Nt);
+        cx_cube G = zeros<cx_cube>(Ns, Ns, Nt);
+        cx_cube f = zeros<cx_cube>(Ns, Ns, Nt);
+
+        double wd = w(2);
+        double a =60.8;
+
+        //Spatiotemporal signal
+        for(int l = 0; l < Nt; l++){
+            for(int i = 0; i < Ns; i++){
+                for(int j = 0; j < Ns; j++){
+                    g(i,j,l) =gauss(a, {s[i], s[j]}) * cos(wd * t[l]);
+                }
+            }
+        }
+
+        //fourier signal
+        for(int l = 0; l < Nt; l++){
+            for(int i = 0; i < Ns; i++){
+                for(int j = 0; j < Ns; j++){
+                    f(i,j,l) = gaussFT(a, {k[i], k[j]})
+                            * Functions::delta(wd, w[l]);
+                }
+            }
+        }
+
+        f *= 2*PI;
+        f /= integrator.temporalFreqResolution();
+
+        // Backward
+        G = integrator.integrate(f);
+        G = FFTHelper::fftShift(G);
+
+        // Test
+        for(int l = 0; l < Nt; l++){
+            for(int i = 0; i < Ns; i++){
+                for(int j = 0; j < Ns; j++){
+                    CHECK_CLOSE(real(g(i,j,l)),
+                                real(G(i,j,l)), 1e-12);
+                }
+            }
+        }
+    }
 
 
 }
