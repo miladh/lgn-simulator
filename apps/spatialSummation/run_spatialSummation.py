aedb62181c9810fd24e6b629114899961377a12e
Milad <miladh@fys.uio.no>
2016-01-19 13:45:58 +0100

    updates

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -20,54 +20,54 @@ args = parser.parse_args()
 
 app_name = "spatialSummation"
 
-# Run edog:---------------------------------------------------------------------
+# Run edog:
 config_file = args.config_file
 run_id = edog_runner.run_edog(app_name, config_file)
 data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Data", run_id)
 
-# Read data:--------------------------------------------------------------------
-data_files = glob.glob1(data_path,'*.h5')
-num_data_files = len(data_files)
-sim = []
-
-for i in range(num_data_files):
-    data_file = os.path.join(data_path, data_files[i])
-    f = h5py.File(data_file, "r")
-    sim.append(Simulation.Simulation(f))
-
-
-# Plotting: --------------------------------------------------------------------
-exp = sim[0]
-
-nPoints = 5
-cells = ["ganglion"]
-idx = np.sort(np.random.randint(0, exp.num_points, nPoints))
-idy = np.sort(np.random.randint(0, exp.num_points, nPoints))
-t = exp.time_vec
-
-fig = mplt.figure(figsize=(16,12))
-n = 0
-for c, cell in enumerate(cells):
-    spikeTrains = []
-    responses = []
-    n+=1
-
-    ax  = fig.add_subplot(len(cells), 2, n)
-    for i,j in zip(idx, idy):
-        label = "(" + str('%.1f' % (i/float(exp.num_points,))) + ", " +  str('%.1f' % (j/float(exp.num_points,)))+ ")"
-        spikeTrains.append([exp.spikeTrain(cell, i, j, num_trails = 1), label])
-        responses.append([exp.singleCellTemporalResponse(cell, i, j), label])
-
-        ax.plot(t, responses[-1][0], label = responses[-1][1],zorder=3)
-        ax.set_xlabel("t[s]", fontsize= 16)
-        ax.set_ylabel("Response",fontsize= 16)
-        ax.set_title(cell, fontsize = 16)
-        ax.legend(frameon=False)
-
-    n+=1
-    ax = fig.add_subplot(len(cells), 2, n)
-    ax = plt.raster(spikeTrains, figsize = (6,6), ax = ax)
-    ax.set_title(cell, fontsize = 16)
-
-mplt.tight_layout()
-fig.savefig(os.path.join(data_path, "rat_cellResponse.png"))
+# # Read data:--------------------------------------------------------------------
+# data_files = glob.glob1(data_path,'*.h5')
+# num_data_files = len(data_files)
+# sim = []
+#
+# for i in range(num_data_files):
+#     data_file = os.path.join(data_path, data_files[i])
+#     f = h5py.File(data_file, "r")
+#     sim.append(Simulation.Simulation(f))
+#
+#
+# # Plotting: --------------------------------------------------------------------
+# exp = sim[0]
+#
+# nPoints = 5
+# cells = ["ganglion"]
+# idx = np.sort(np.random.randint(0, exp.num_points, nPoints))
+# idy = np.sort(np.random.randint(0, exp.num_points, nPoints))
+# t = exp.time_vec
+#
+# fig = mplt.figure(figsize=(16,12))
+# n = 0
+# for c, cell in enumerate(cells):
+#     spikeTrains = []
+#     responses = []
+#     n+=1
+#
+#     ax  = fig.add_subplot(len(cells), 2, n)
+#     for i,j in zip(idx, idy):
+#         label = "(" + str('%.1f' % (i/float(exp.num_points,))) + ", " +  str('%.1f' % (j/float(exp.num_points,)))+ ")"
+#         spikeTrains.append([exp.spikeTrain(cell, i, j, num_trails = 1), label])
+#         responses.append([exp.singleCellTemporalResponse(cell, i, j), label])
+#
+#         ax.plot(t, responses[-1][0], label = responses[-1][1],zorder=3)
+#         ax.set_xlabel("t[s]", fontsize= 16)
+#         ax.set_ylabel("Response",fontsize= 16)
+#         ax.set_title(cell, fontsize = 16)
+#         ax.legend(frameon=False)
+#
+#     n+=1
+#     ax = fig.add_subplot(len(cells), 2, n)
+#     ax = plt.raster(spikeTrains, figsize = (6,6), ax = ax)
+#     ax.set_title(cell, fontsize = 16)
+#
+# mplt.tight_layout()
+# fig.savefig(os.path.join(data_path, "rat_cellResponse.png"))
