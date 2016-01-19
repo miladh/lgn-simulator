4beef3ef7fc7d4d0e45dae0344a469d3ef3173d2
Milad <miladh@fys.uio.no>
2016-01-19 11:16:11 +0100

    updates

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import os, sys
+import h5py
+from glob import glob
 from sys import argv
 from argparse import ArgumentParser
 current_path = os.path.dirname(os.path.realpath(__file__))
@@ -8,13 +10,31 @@ sys.path.append(lib_path)
 
 import Edog_runner as edog_runner
 
+
+
 parser = ArgumentParser()
-parser.add_argument("config_file")
+parser.add_argument("run_edog", default=False)
+parser.add_argument("config_file", default=None)
+parser.add_argument("data_path", default=None)
 args = parser.parse_args()
 
-config_file = args.config_file
 app_name = "spatialSummation"
 
-edog_runner.run_edog(app_name, config_file)
+if args.run_edog
+    print "Running edog..."
+    config_file = args.config_file
+    edog_runner.run_edog(app_name, config_file)
+
+if data_path==None:
+    print "Cannot find data file..."
+
+data_files = glob.glob1(data_path,'*.h5')
+num_data_files = len(data_files)
+sim = []
+for i in range(num_data_files):
+    f = h5py.File(data_files[i], "r")
+    sim.append(Simulation(f))
+
+
 
 #Plotting
