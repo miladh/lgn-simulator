1a17dd1aa912d07dc1d514a7eecef0637ecd0c83
Milad <miladh@fys.uio.no>
2016-01-19 11:44:06 +0100

    updates

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -47,12 +47,45 @@ for i in range(num_data_files):
 exp = sim[0]
 
 #Plotting
-data = [
-     [exp.stimulus["spatioTemporal"], "Stimulus"]
-    ,[exp.ganglion["response"]["spatioTemporal"], "Ganglion cell response"]
-    ,[exp.ganglion["impulseResponse"]["spatioTemporal"], "Ganglion cell impulse response"]
-    ]
+# data = [
+#      [exp.stimulus["spatioTemporal"], "Stimulus"]
+#     ,[exp.ganglion["response"]["spatioTemporal"], "Ganglion cell response"]
+#     ,[exp.ganglion["impulseResponse"]["spatioTemporal"], "Ganglion cell impulse response"]
+#     ]
+# plt.animateImshowPlots(data,exp.dt, colorbar = True, save_animation = False, animation_name = "rat")
+# mplt.show()
 
 
-plt.animateImshowPlots(data,exp.dt, colorbar = True, save_animation = False, animation_name = "rat")
-mplt.show()
+
+nPoints = 5
+cells = ["ganglion"]
+idx = sort(random.randint(0, exp.num_points, nPoints))
+idy = sort(random.randint(0, exp.num_points, nPoints))
+t = exp.time_vec
+
+fig = figure(figsize=(16,12))
+n = 0
+for c, cell in enumerate(cells):
+    spikeTrains = []
+    responses = []
+    n+=1
+
+    ax  = fig.add_subplot(len(cells), 2, n)
+    for i,j in zip(idx, idy):
+        label = "(" + str('%.1f' % (i/float(exp.num_points,))) + ", " +  str('%.1f' % (j/float(exp.num_points,)))+ ")"
+        spikeTrains.append([exp.spikeTrain(cell, i, j, num_trails = 1), label])
+        responses.append([exp.singleCellTemporalResponse(cell, i, j), label])
+
+        ax.plot(t, responses[-1][0], label = responses[-1][1],zorder=3)
+        ax.set_xlabel("t[s]", fontsize= 16)
+        ax.set_ylabel("Response",fontsize= 16)
+        ax.set_title(cell, fontsize = 16)
+        ax.legend(frameon=False)
+
+    n+=1
+    ax = fig.add_subplot(len(cells), 2, n)
+    ax = pl.raster(spikeTrains, figsize = (6,6), ax = ax)
+    ax.set_title(cell, fontsize = 16)
+
+tight_layout()
+fig.savefig("rat_cellResponse.pdf")
