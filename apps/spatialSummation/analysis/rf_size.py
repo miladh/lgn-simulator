71e04e5086d169bbde0e4d3afddc9f6292cb7a25
Milad <miladh@fys.uio.no>
2016-01-20 17:22:39 +0100

    updates

--- apps/spatialSummation/analysis/rf_size.py
+++ apps/spatialSummation/analysis/rf_size.py
@@ -21,36 +21,27 @@ sims, output_dir = get_simulations.get_simulation_environment(config_file)
 # Analysis: --------------------------------------------------------------------
 exp = sims[0]
 
-nPoints = 5
 cells = ["ganglion"]
-idx = np.sort(np.random.randint(0, exp.num_points, nPoints))
-idy = np.sort(np.random.randint(0, exp.num_points, nPoints))
+idx = exp.num_points * 0.5
+idy = idx
 t = exp.time_vec
 
 fig = mplt.figure(figsize=(16,12))
 n = 0
 for c, cell in enumerate(cells):
-    spikeTrains = []
     responses = []
     n+=1
 
     ax  = fig.add_subplot(len(cells), 2, n)
-    for i,j in zip(idx, idy):
-        label = "(" + str('%.1f' % (i/float(exp.num_points,))) + ", " +  str('%.1f' % (j/float(exp.num_points,)))+ ")"
-        spikeTrains.append([exp.spikeTrain(cell, i, j, num_trails = 1), label])
-        responses.append([exp.singleCellTemporalResponse(cell, i, j), label])
+    responses.append([exp.singleCellTemporalResponse(cell, idx, idy), "ganglion"])
 
-        ax.plot(t, responses[-1][0], label = responses[-1][1],zorder=3)
-        ax.set_xlabel("t[s]", fontsize= 16)
-        ax.set_ylabel("Response",fontsize= 16)
-        ax.set_title(cell, fontsize = 16)
-        ax.legend(frameon=False)
+    ax.plot(t, responses[-1][0], label = responses[-1][1],zorder=3)
+    ax.set_xlabel("t[s]", fontsize= 16)
+    ax.set_ylabel("Response",fontsize= 16)
+    ax.set_title(cell, fontsize = 16)
+    ax.legend(frameon=False)
 
     n+=1
-    ax = fig.add_subplot(len(cells), 2, n)
-    ax = plt.raster(spikeTrains, figsize = (6,6), ax = ax)
-    ax.set_title(cell, fontsize = 16)
-
 mplt.tight_layout()
 mplt.show()
 fig.savefig(os.path.join(output_dir, "rat_cellResponse.png"))
