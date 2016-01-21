5d3b48a38727eb90e436f8f87ff276df0c562c7f
Milad <miladh@fys.uio.no>
2016-01-21 09:30:47 +0100

    updates

--- apps/spatialSummation/analysis/rf_size.py
+++ apps/spatialSummation/analysis/rf_size.py
@@ -15,7 +15,7 @@ parser = ArgumentParser()
 parser.add_argument("config_file", default=None)
 args = parser.parse_args()
 config_file = args.config_file
-sims, output_dir = get_simulations.get_simulation_environment(config_file, False)
+sims, output_dir = get_simulations.get_simulation_environment(config_file, record=True)
 
 
 # Analysis: --------------------------------------------------------------------
@@ -35,4 +35,4 @@ fig = mplt.figure(figsize=(16,12))
 # set_ylabel("Response",fontsize= 16)
 # mplt.tight_layout()
 mplt.show()
-# fig.savefig(os.path.join(output_dir, "rat_cellResponse.png"))
+fig.savefig(os.path.join(output_dir, "rat_cellResponse.png"))
