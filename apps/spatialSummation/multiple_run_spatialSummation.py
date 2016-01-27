a7ba51649aef60c42c071429388a849074b0aaf8
Milad <miladh@fys.uio.no>
2016-01-27 13:19:07 +0100

    added new script

---
+++ apps/spatialSummation/multiple_run_spatialSummation.py
@@ -0,0 +1,7 @@
+from subprocess import call
+import numpy as np
+
+spot_diameters = np.linspace(0, 0.8, 20)
+
+for d in spot_diameters:
+    call(["smt", "run", "spatialSummationConfig.yaml", "maskSize="+str(d)])
