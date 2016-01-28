e68f056b9e0f5aae088b9442fc01d9b3c43b3908
Milad <miladh@fys.uio.no>
2016-01-28 16:30:03 +0100

    new param for x-cells

--- apps/nonlaggedXCells/analysis/rf_size.py
+++ apps/nonlaggedXCells/analysis/rf_size.py
@@ -3,6 +3,7 @@ import os, sys
 from argparse import ArgumentParser
 import numpy as np
 import matplotlib.pyplot as mplt
+from scipy import io
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 lib_path = os.path.abspath(os.path.join(current_path,"..", "..","..","tools"))
@@ -17,28 +18,58 @@ args = parser.parse_args()
 config_file = args.config_file
 
 
-sims, output_dir=get_simulations.get_simulation_environment(config_file, record=True)
+sims, output_dir=get_simulations.get_simulation_environment(config_file, record=False)
+
+
+# Get experimental data: -------------------------------------------------------
+exp_data_path = os.path.abspath(os.path.join(current_path,"../../../.." ,"expDATA"))
+exp_data_filename = "nonLaggedXCells.mat"
+exp_data = io.loadmat(os.path.join(exp_data_path, exp_data_filename ))
+relay_exp = [np.array(exp_data["xrelay_data"]).transpose()[0]*2.0,
+            np.array(exp_data["yrelay_data"]).transpose()[0]]
+ganglion_exp = [np.array(exp_data["xgang_data"]).transpose()[0]*2.0,
+               np.array(exp_data["ygang_data"]).transpose()[0]]
 
 
 # Analysis: --------------------------------------------------------------------
 cell_pos_x = 0.5
 cell_pos_y = cell_pos_x
 
-types = ["ganglion", "relay"]
-responses = np.zeros([len(types), len(sims)])
-spot_diameter = np.zeros(len(sims))
+data = {"ganglion": {"spot_diameter": np.zeros(len(sims)),
+                    "responses": np.zeros(len(sims)) }
+        ,"relay":   {"spot_diameter": np.zeros(len(sims)),
+                            "responses": np.zeros(len(sims)) }}
 
-fig = mplt.figure(figsize=(8,6))
-for i, t in enumerate(types):
+for cell in data:
     for j, exp in enumerate(sims):
         idx = exp.num_points * cell_pos_x
         idy = exp.num_points * cell_pos_y
-        spot_diameter[j] = exp.stimulus.maskSize
-        responses[i,j] = np.mean(exp.singleCellTemporalResponse(t, idx, idy))
+        data[cell]["spot_diameter"][j] = exp.stimulus.maskSize
+        data[cell]["responses"][j] = np.mean(
+        exp.singleCellTemporalResponse(cell, idx, idy))
 
-    mplt.plot(spot_diameter, responses[i], "o-", label = t)
 
-mplt.xlim(0., 1.)
+# Rescale
+data["ganglion"]["spot_diameter"] *= 10
+data["ganglion"]["responses"] *= 121
+data["ganglion"]["responses"] += 39
+
+data["relay"]["spot_diameter"] *= 10
+data["relay"]["responses"] *= 121
+data["relay"]["responses"] += 4
+
+
+# Plot:
+mplt.plot(data["ganglion"]["spot_diameter"], data["ganglion"]["responses"], "r-", label = "ganglion")
+mplt.plot(data["relay"]["spot_diameter"], data["relay"]["responses"], "b-", label = "relay")
+
+# Plot experimental data:
+mplt.plot(ganglion_exp[0], ganglion_exp[1], "rv")
+mplt.plot(relay_exp[0], relay_exp[1], "bv")
+
+
+
+# mplt.xlim(0., 1.)
 mplt.xlabel("Spot diameter", fontsize= 16)
 mplt.ylabel("Response",fontsize= 16)
 mplt.tight_layout()
