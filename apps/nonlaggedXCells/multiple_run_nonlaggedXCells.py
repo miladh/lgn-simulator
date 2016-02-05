d6738d6923dc92fcc1cdd56840bcd51937080e9b
Milad <miladh@fys.uio.no>
2016-02-05 15:31:06 +0100

    new param for nonlagged cells

--- apps/nonlaggedXCells/multiple_run_nonlaggedXCells.py
+++ apps/nonlaggedXCells/multiple_run_nonlaggedXCells.py
@@ -1,7 +1,7 @@
 from subprocess import call
 import numpy as np
 
-spot_diameters = np.linspace(0, 0.9, 40)
+spot_diameters = np.linspace(0, 0.9, 10)
 
 for d in spot_diameters:
     call(["smt", "run", "nonlaggedXCellsConfig.yaml", "maskSize="+str(d)])
