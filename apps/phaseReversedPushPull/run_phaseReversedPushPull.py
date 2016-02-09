6ce214b68e36876fb0b56d94bf2fbb7c2f46ead1
Milad <miladh@fys.uio.no>
2016-02-09 11:44:58 +0100

    renaming

--- apps/phaseReversedPushPull/run_phaseReversedPushPull.py
+++ apps/phaseReversedPushPull/run_phaseReversedPushPull.py
@@ -6,7 +6,7 @@ current_path = os.path.dirname(os.path.realpath(__file__))
 lib_path = os.path.abspath(os.path.join(current_path, "..","..","tools"))
 sys.path.append(lib_path)
 
-import edog_runner
+import lgnSimulator_runner
 
 parser = ArgumentParser()
 parser.add_argument("config_file", default=None)
@@ -16,4 +16,4 @@ app_name = os.path.basename(current_path)
 
 # Run edog:
 config_file = args.config_file
-run_id = edog_runner.run_edog(app_name, config_file)
+run_id = lgnSimulator_runner.run_lgnSimulator(app_name, config_file)
