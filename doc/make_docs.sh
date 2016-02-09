8778d853029fbbdaeea791156e2c4d7ef0acf1d5
Milad <miladh@fys.uio.no>
2016-02-09 17:03:49 +0100

    updated doc files

--- doc/make_docs.sh
+++ doc/make_docs.sh
@@ -2,10 +2,10 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOTDIR=$DIR/../..
-DOCSDIR=$DIR/../../edog-docs
+DOCSDIR=$DIR/../../lgn-simulator-docs
 
 
 
 cd $DIR
-echo Running $DOCSDIR/qdoc edog.qdocconf
-LD_LIBRARY_PATH=$DOCSDIR $DOCSDIR/qdoc edog.qdocconf
+echo Running $DOCSDIR/qdoc lgn-simulator.qdocconf
+LD_LIBRARY_PATH=$DOCSDIR $DOCSDIR/qdoc lgn-simulator.qdocconf
