b267b94917d7d2cbfda1f4c8fe4dceafd7d39212
Milad <miladh@fys.uio.no>
2016-02-09 14:24:48 +0100

    updates

--- tools/sumatraTracking/run_app.py
+++ tools/sumatraTracking/run_app.py
@@ -25,7 +25,7 @@ def run_simulator(app_name, config_file):
     with open(config_file, 'w') as stream:
         yaml.dump(config_data, stream)
 
-    build_path = os.path.abspath(os.path.join(current_path, "../../.. , "build"))
+    build_path = os.path.abspath(os.path.join(current_path, "../../.." , "build"))
     project_path = os.path.abspath(os.path.join(current_path, "../.."))
 
     print "Building in:\n", build_path
