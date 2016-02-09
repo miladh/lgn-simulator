b4064f90513ed9acddf17f8a6125a05fcf4635a5
Milad <miladh@fys.uio.no>
2016-02-09 15:59:37 +0100

    updates

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -4,4 +4,5 @@ import numpy as np
 spot_diameters = np.linspace(0, 0.9, 1)
 
 for d in spot_diameters:
-    call(["smt", "run", "spatialSummation.yaml", "maskSize="+str(d)])
+    a = call(["smt", "run", "spatialSummation.yaml", "maskSize="+str(d)])
+    print a
