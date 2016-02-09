1a0f7c52aee5ccbbe1a73fa6e0789caad586857c
Milad <miladh@fys.uio.no>
2016-02-09 16:03:15 +0100

    updates

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -4,5 +4,6 @@ import numpy as np
 spot_diameters = np.linspace(0, 0.9, 1)
 
 for d in spot_diameters:
-    process = subprocess.call(["smt", "run", "spatialSummation.yaml", "maskSize="+str(d)])
+    process = subprocess.Popen(["smt", "run", "spatialSummation.yaml", "maskSize="+str(d)],
+                stdout=subprocess.PIPE)
     print process.communicate()[0]
