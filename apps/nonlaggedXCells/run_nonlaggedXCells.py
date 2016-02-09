61853ffceb43b9172357d5f413b82657a9fb40d3
Milad <miladh@fys.uio.no>
2016-02-09 16:44:10 +0100

    updates

--- apps/nonlaggedXCells/run_nonlaggedXCells.py
+++ apps/nonlaggedXCells/run_nonlaggedXCells.py
@@ -1,7 +1,7 @@
 from subprocess import call
 import numpy as np
 
-spot_diameters = np.linspace(0, 0.9, 10)
+spot_diameters = np.linspace(0, 0.9, 30)
 
 for d in spot_diameters:
     call(["smt", "run", "nonlaggedXCells.yaml", "maskSize="+str(d)])
