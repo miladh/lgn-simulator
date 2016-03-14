75e98a44d5d28d356867f3dd27add9326ed034b7
Milad <miladh@fys.uio.no>
2016-03-14 17:12:04 +0100

    rotating stim working

--- src/stimuli/grating/fullfieldgrating.cpp
+++ src/stimuli/grating/fullfieldgrating.cpp
@@ -27,33 +27,34 @@ double FullFieldGrating::valueAtPoint(vec2 rVec, double t) const
 //    k_rot(1) = cos(phi)*m_k(1) + sin(phi)*m_k(0);
 
 
-//    double phi = core::pi/3;
-//    double k_mag = sqrt(dot(m_k, m_k));
-//    vec2 k_rot = {k_mag*cos(phi), k_mag*sin(phi)};
-//    double s = m_contrast * cos(dot(k_rot, rVec) - m_w * t);
+    double phi = 2.*core::pi/5;
+    double k_mag = sqrt(dot(m_k, m_k));
+    vec2 k_rot = {k_mag*cos(phi), k_mag*sin(phi)};
+    double s = m_contrast * cos(dot(k_rot, rVec) - m_w * t);
 
-        double s = m_contrast * cos(dot(m_k, rVec) - m_w * t );
+//        double s = m_contrast * cos(dot(m_k, rVec) - m_w * t );
     return s;
 }
 
 complex<double> FullFieldGrating::fourierTransformAtFrequency(vec2 k, double w) const
 {
-//    double phi = core::pi/3;
-//    double k_mag = sqrt(dot(m_k, m_k));
-//    vec2 k_rot = {k_mag*cos(phi), k_mag*sin(phi)};
-
-//    k_rot(0) = Special::nearestValue(m_spatialFreqs, k_rot(0));
-//    k_rot(1) = Special::nearestValue(m_spatialFreqs, k_rot(1));
+    double phi = 2.*core::pi/5;
+    double k_mag = sqrt(dot(m_k, m_k));
+    vec2 k_rot = {k_mag*cos(phi), k_mag*sin(phi)};
 
 //    cout << k_rot.t() << endl;
 
-//    double s = (Special::delta(k, k_rot) * Special::delta(w,m_w)
-//                + Special::delta(k, -k_rot) * Special::delta(w, -m_w));
+    k_rot(0) = Special::nearestValue(m_spatialFreqs, k_rot(0));
+    k_rot(1) = Special::nearestValue(m_spatialFreqs, k_rot(1));
+
+//    cout << k_rot.t() << endl <<endl;
+    double s = (Special::delta(k, k_rot) * Special::delta(w,m_w)
+                + Special::delta(k, -k_rot) * Special::delta(w, -m_w));
 
 
 
-    double s = (Special::delta(k, m_k) * Special::delta(w,m_w)
-                + Special::delta(k, -m_k) * Special::delta(w, -m_w));
+//    double s = (Special::delta(k, m_k) * Special::delta(w,m_w)
+//                + Special::delta(k, -m_k) * Special::delta(w, -m_w));
 
     return 4.*core::pi*core::pi*core::pi * m_contrast * m_peak * s;
 }
