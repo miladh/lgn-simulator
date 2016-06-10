752e0f41a5bf173dfda0d22151aa9e4b9cdb0448
Milad <miladh@fys.uio.no>
2016-06-10 15:57:57 +0200

    updates

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -10,6 +10,7 @@ sys.path.append(os.path.abspath(os.path.join(current_path,"../../tools")))
 import sumatra_tracking.run_app as st
 
 def modify_diameter(d):
+    stream.write("# variable")
     config_data["stimulus"]["maskSize"] = float(d)
 
 def modify_Kic(w):
