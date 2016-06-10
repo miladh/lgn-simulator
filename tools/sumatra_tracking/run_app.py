12bf8faf8dca3efa1fc43ae1848cdc7b4612d2b3
Milad <miladh@fys.uio.no>
2016-06-10 15:01:01 +0200

    updates

--- tools/sumatra_tracking/run_app.py
+++ tools/sumatra_tracking/run_app.py
@@ -21,7 +21,7 @@ def run_simulator(config_file, record_label, run_id):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    output_file = os.path.join(output_dir, record_label, run_id + ".h5")
+    output_file = os.path.join(output_dir, record_label+ "_"+run_id + ".h5")
     print "output_file: ", output_file
 
     config_data["OutputManager"]["outputFilename"] = unicode(output_file)
