67283b62ff6d26b09fa3a149afd244c458e44fb1
Milad <miladh@fys.uio.no>
2016-06-13 09:04:25 +0200

    updates

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -22,39 +22,42 @@ def modify_Krc(w):
 def modify_Kri(w):
     config_data["relay"]["Kri"]["spatial"]["weight"]= float(-w)
 
-if __name__ == "__main__":
-    options = sys.argv[1:]
-    record_label = options[-1]
-    config_file = os.path.abspath(os.path.join(current_path,"spatialSummation.yaml"))
 
-    spot_diameters = np.linspace(0, 0.9, 2)
-    weights = np.linspace(0.0, 2.0, 10)
+options = sys.argv[1:]
+record_label = options[-1]
+config_file = os.path.abspath(os.path.join(current_path,"spatialSummation.yaml"))
 
-    with open(config_file, 'r') as stream:
-        config_data = yaml.load(stream)
+spot_diameters = np.linspace(0, 0.9, 2)
+weights = np.linspace(0.0, 2.0, 10)
+
+with open(config_file, 'r') as stream:
+    config_data = yaml.load(stream)
 
 
-    counter= 0
-    # for w in weights:
-    #     modify_Kic(w)
-    #     modify_Krc(w)
+counter= 0
+# for w in weights:
+#     modify_Kic(w)
+#     modify_Krc(w)
 
-        # modify_Kri(w)
+    # modify_Kri(w)
 
-    for d in spot_diameters:
-        modify_diameter(d)
+for d in spot_diameters:
+    modify_diameter(d)
 
-        run_id = '{0:04}'.format(counter)
-        with open(config_file, 'w') as stream:
-            yaml.dump(config_data, stream)
+    with open(config_file, 'w') as stream:
+        yaml.dump(config_data, stream)
 
-        copyfile(os.path.abspath(os.path.join(current_path,"spatialSummation.yaml")),
-                 os.path.abspath(os.path.join(current_path, record_label+"_"+run_id +".yaml")))
+    run_id = '{0:04}'.format(counter)
+    tmp_config_filename = record_label+"_"+run_id +".yaml"
 
-        st.run_simulator(config_file, record_label, run_id)
-        counter+=1
+    copyfile(os.path.abspath(os.path.join(current_path,"spatialSummation.yaml")),
+             os.path.abspath(os.path.join(current_path, tmp_config_filename)))
+    tmp_config_file = os.path.abspath(os.path.join(current_path, tmp_config_filename))
 
-    os.remove(config_file)
+    st.run_simulator(config_file, record_label, run_id)
+    os.remove(tmp_config_file)
+
+    counter+=1
 
 
 # if __name__ == "__main__":
