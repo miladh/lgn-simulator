ac120647ceebd915981f675de29565bd7e9c5591
Milad <miladh@fys.uio.no>
2016-07-14 19:33:05 +0200

    new helper functions

--- src/helper/ffthelper.cpp
+++ src/helper/ffthelper.cpp
@@ -63,6 +63,20 @@ cx_mat FFTHelper::fftShift(cx_mat x)
     return shifted;
 }
 
+mat FFTHelper::fftShift(mat x)
+{
+    int n = x.n_rows;
+    int n2 = int((n+1)/2);
+
+    int m = x.n_cols;
+    int m2 = int((m+1)/2);
+
+    mat shifted = join_vert(x.rows(n2, n-1), x.rows(0,n2-1));
+    shifted = join_horiz(shifted.cols(m2, m-1), shifted.cols(0,m2-1));
+
+    return shifted;
+}
+
 
 cx_mat FFTHelper::ifftShift(cx_mat x)
 {
@@ -77,6 +91,22 @@ cx_mat FFTHelper::ifftShift(cx_mat x)
 
     return shifted;
 }
+
+
+mat FFTHelper::ifftShift(mat x)
+{
+    int n = x.n_rows;
+    int n2 = n-int((n+1)/2);
+
+    int m = x.n_cols;
+    int m2 = m-int((m+1)/2);
+
+    mat shifted = join_vert(x.rows(n2, n-1), x.rows(0,n2-1));
+    shifted = join_horiz(shifted.cols(m2, m-1), shifted.cols(0,m2-1));
+
+    return shifted;
+}
+
 
 
 cx_cube FFTHelper::fftShift(cx_cube x)
@@ -138,5 +168,80 @@ cx_cube FFTHelper::ifftShift(cx_cube x)
 
     return shifted;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+cube FFTHelper::fftShift(cube x)
+{
+    cube shifted = x;
+    int n = x.n_rows;
+    int n2 = int((n+1)/2);
+
+    int m = x.n_cols;
+    int m2 = int((m+1)/2);
+
+    int o = x.n_slices;
+    int o2 = int((o+1)/2);
+
+    for(int i = 0; i < int(x.n_slices); i++){
+        shifted.slice(i) = join_vert(x.slice(i).rows(n2, n-1),
+                                     x.slice(i).rows(0,n2-1));
+        shifted.slice(i) = join_horiz(shifted.slice(i).cols(m2, m-1),
+                                      shifted.slice(i).cols(0,m2-1));
+    }
+
+    cube tmp = shifted;
+    shifted.slices(0, (o-1) - o2) = tmp.slices(o2, o-1);
+    shifted.slices((o-1-o2)+1, (o-1-o2)+1+(o2-1)) = tmp.slices(0, o2-1);
+    return shifted;
+}
+
+
+
+
+
+
+cube FFTHelper::ifftShift(cube x)
+{
+    cube shifted = x;
+    int n = x.n_rows;
+    int n2 = n-int((n+1)/2);
+
+    int m = x.n_cols;
+    int m2 = m-int((m+1)/2);
+
+    int o = x.n_slices;
+    int o2 = o-int((o+1)/2);
+
+
+
+    for(int i = 0; i < int(x.n_slices); i++){
+        shifted.slice(i) = join_vert(x.slice(i).rows(n2, n-1),
+                                     x.slice(i).rows(0,n2-1));
+        shifted.slice(i) = join_horiz(
+                    shifted.slice(i).cols(m2, m-1),
+                    shifted.slice(i).cols(0,m2-1));
+    }
+
+    cube tmp = shifted;
+    shifted.slices(0, (o-1) - o2) = tmp.slices(o2, o-1);
+    shifted.slices((o-1-o2)+1, (o-1-o2)+1+(o2-1)) = tmp.slices(0, o2-1);
+
+
+    return shifted;
+}
+
+
+
 
 
--- src/helper/ffthelper.h
+++ src/helper/ffthelper.h
@@ -19,11 +19,19 @@ public:
     static cx_vec fftShift(cx_vec x);
     static vec fftShift(vec x);
 
+    static mat fftShift(mat x);
+    static cube fftShift(cube x);
+
     static cx_mat fftShift(cx_mat x);
     static cx_cube fftShift(cx_cube x);
 
-    static cx_cube ifftShift(cx_cube x);
+
+    static mat ifftShift(mat x);
+    static cube ifftShift(cube x);
+
     static cx_mat ifftShift(cx_mat x);
+    static cx_cube ifftShift(cx_cube x);
+
 
 };
 }
