726616e382ec49e0642963184a16305bd4bad9f5
Milad <miladh@ibv.uio.no>
2016-08-15 10:50:41 +0200

    updates

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -9,6 +9,22 @@ current_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(os.path.join(current_path,"../../tools")))
 import sumatra_tracking.run_simulator as st
 
+
+def run_simulation(attrs):
+    counter= 0
+    for attr in attrs:
+        for value in attr[values]:
+            attr["func"](value)
+            with open(config_file, 'w') as stream:
+                yaml.dump(config_data, stream)
+
+            run_id = '{0:04}'.format(counter)
+            st.run_simulator(config_file, record_label, run_id)
+            counter+=1
+
+    os.remove(config_file)
+
+
 def modify_phase(phase):
     config_data["stimulus"]["phase"] = float(phase)
 
@@ -41,43 +57,16 @@ with open(config_file, 'r') as stream:
     config_data = yaml.load(stream)
 
 
-#parameters-------------------------------------------------------------------------------------
-spot_diameters = np.linspace(0., 15, 50)
-# spot_diameters = [13.734940]
-spatial_freqs = 4
-weights = [0.0, 0.2, 0.4, 0.6, 0.8]
-phase = np.linspace(-180, 180, 10)
-
-# #run simulator----------------------------------------------------------------------------------
-# counter= 0
-# for Kc in weights:
-#     modify_Krc(Kc)
-#     modify_Kic(Kc)
-#     for d in spot_diameters:
-#         modify_diameter(d)
-#         with open(config_file, 'w') as stream:
-#             yaml.dump(config_data, stream)
-#
-#         run_id = '{0:04}'.format(counter)
-#         st.run_simulator(config_file, record_label, run_id)
-#         counter+=1
-
 # #parameters-------------------------------------------------------------------------------------
 spot_diameters = np.linspace(0., 15, 1)
 spatial_freqs = 4
 weights = np.linspace(0, 1.0, 6)
 phase = np.linspace(-180, 180, 10)
-
 widths = np.array([0.25, 0.5, 1.0, 2.0])*0.1
+
+attrs = {
+    "a_rc" : {"func": modify_arc, "values": widths}
+}
+
 #run simulator----------------------------------------------------------------------------------
-counter= 0
-for a in widths:
-    modify_arc(a)
-    with open(config_file, 'w') as stream:
-        yaml.dump(config_data, stream)
-
-    run_id = '{0:04}'.format(counter)
-    st.run_simulator(config_file, record_label, run_id)
-    counter+=1
-
-os.remove(config_file)
+run_simulation()
