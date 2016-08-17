0835ddd3e3feb63552a881f82a38f5a3b0970969
Milad <miladh@ibv.uio.no>
2016-08-17 15:43:38 +0200

    updates

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -60,22 +60,23 @@ with open(config_file, 'r') as stream:
 
 
 #parameters-------------------------------------------------------------------------------------
-spot_diameters = np.linspace(0., 15, 250)
-weights = np.array([0.0, 0.2, 0.4, 0.8, 0.9, 1.2, 2.0])
+spot_diameters = np.linspace(0., 15, 50)
+weights = np.array([0.0,  0.4, 0.9, 1.2])
 widths = np.array([0.5, 1.0, 2.0, 5.0, 10., 20.])
 
 
 #run simulator----------------------------------------------------------------------------------
 counter= 0
+modify_aig(0.5)
+modify_aic(1.0)
+modify_arc(0.1)
 
 for w in weights:
     modify_wig(1)
     modify_wic(w)
     modify_wrc(w)
-    # for a in widths:
-    modify_aig(0.5)
-    modify_aic(1.0)
-    modify_arc(0.1)
+    for d in spot_diameters:
+        modify_diameter(d)
 ##########################################################
         with open(config_file, 'w') as stream:
             yaml.dump(config_data, stream)
