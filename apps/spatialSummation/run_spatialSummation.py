7ecacb9167b9c1bdc9449314c10b6f5ecc71f415
Milad <miladh@ibv.uio.no>
2016-08-17 11:19:16 +0200

    updates

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -54,7 +54,7 @@ with open(config_file, 'r') as stream:
 
 #parameters-------------------------------------------------------------------------------------
 spot_diameters = np.linspace(0., 15, 20)
-weights = np.array([0.0,  2.])
+weights = np.array([0.0,  1.5])
 widths = np.array([0.5, 1.0, 2.0, 4.0, 10.])
 
 
