c62b11bb3dcc447f2e709a6b38cf008ed6dee131
Milad <miladh@ibv.uio.no>
2016-09-15 09:29:21 +0200

    new routine for smt analysis

--- apps/spatialSummation/analysis/irf_spatial.py
+++ apps/spatialSummation/analysis/irf_spatial.py
@@ -1,52 +1,13 @@
 #!/usr/bin/python
+from pylab import*
 import os, sys
 current_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(os.path.join(current_path,"../../../tools")))
-import sumatra_tracking.io_manager as smt
-
-record_label = sys.argv[1:][-1]
-sims_path = sys.argv[1:][-2]
-output_dir = smt.get_output_dir(record_label)
+from analysis.data_extractor import*
 
 #Analysis: ###########################################################################
-from pylab import*
-import seaborn.apionly as sns
-sns.set_color_codes()
-from analysis.data_extractor import*
-from analysis.pretty_plotting import*
-
-cell_type = ["relay", "interneuron"]
-attr_a_name = "interneuron.Kic.w"
-attr_b_name = "relay.Krc.w"
-
-xlabel ="$|w_{\mathrm{RC}}|$" #attr_b
-ylabel = "$|w_{\mathrm{IC}}|$" #attr_a
-fig_name= "irf_FB_"
-
-sims = get_simulations(sims_path)
-Ns=sims[0].integrator.Ns
-Nt=sims[0].integrator.Nt
-s_points = sims[0].integrator.s_points[Ns/2:]
-k_points = sims[0].integrator.k_points[Ns/2:]
-
-attr_a = extract_unique_simulation_attrs(sims, attr_a_name)
-attr_a = attr_a[argsort(abs(attr_a))]
-attr_a = attr_a[:]
-
-attr_b = extract_unique_simulation_attrs(sims, attr_b_name)
-attr_b = attr_b[argsort(abs(attr_b))]
-attr_b = attr_b[:]
-
-norm_sim = simulation_extractor(simulation_extractor(sims, attr_a_name, 0), attr_b_name, 0)[0]
-
-
-print "attr_a=", attr_a
-print "attr_b=", attr_b
-
-#--------------------------------------------------------------------------------------
-
-#--------------------------------------------------------------------------------------
 def extract_irfs(cell_type):
+    global attr_a, attr_b
     irf_max = zeros([len(attr_a), len(attr_b)])
     irf_min = zeros([len(attr_a), len(attr_b)])
     irf_size = zeros([len(attr_a), len(attr_b)])
@@ -67,6 +28,9 @@ def extract_irfs(cell_type):
 
 
 def make_plot(irf_max, irf_min, irf_size, cell_type):
+    import seaborn.apionly as sns
+    sns.set_color_codes()
+    from analysis.pretty_plotting import*
 
     fig, axarr = plt.subplots(1,3, figsize=(15,5), sharey='row')
     set_legend()
@@ -112,5 +76,41 @@ def make_plot(irf_max, irf_min, irf_size, cell_type):
     plt.show()
 
 
-for cell in cell_type:
-    make_plot(*extract_irfs(cell), cell_type=cell)
+if __name__ == "__main__":
+    import sumatra_tracking.io_manager as smt
+    record_label = sys.argv[1:][-1]
+    sims_path = sys.argv[1:][-2]
+    output_dir = smt.get_output_dir(record_label)
+    #-----------------------------------------------------------------------------------
+
+    cell_type = ["relay", "interneuron"]
+    attr_a_name = "interneuron.Kic.w"
+    attr_b_name = "relay.Krc.w"
+
+    xlabel ="$|w_{\mathrm{RC}}|$" #attr_b
+    ylabel = "$|w_{\mathrm{IC}}|$" #attr_a
+    fig_name= "irf_FB_"
+
+    sims = get_simulations(sims_path)
+    Ns=sims[0].integrator.Ns
+    Nt=sims[0].integrator.Nt
+    s_points = sims[0].integrator.s_points[Ns/2:]
+    k_points = sims[0].integrator.k_points[Ns/2:]
+
+    attr_a = extract_unique_simulation_attrs(sims, attr_a_name)
+    attr_a = attr_a[argsort(abs(attr_a))]
+    attr_a = attr_a[:]
+
+    attr_b = extract_unique_simulation_attrs(sims, attr_b_name)
+    attr_b = attr_b[argsort(abs(attr_b))]
+    attr_b = attr_b[:]
+
+    norm_sim = simulation_extractor(simulation_extractor(sims, attr_a_name, 0), attr_b_name, 0)[0]
+
+
+    print "attr_a=", attr_a
+    print "attr_b=", attr_b
+
+
+    for cell in cell_type:
+        make_plot(*extract_irfs(cell), cell_type=cell)
