9f7c6c4b2d02a206d101666d6b01c1fd49f40df9
Milad <miladh@ibv.uio.no>
2016-09-17 14:13:17 +0200

    freq tuning

--- apps/spatialSummation/run_spatialSummation.py
+++ apps/spatialSummation/run_spatialSummation.py
@@ -62,16 +62,16 @@ with open(config_file, 'r') as stream:
 #parameters---------------------------------------------------------------------
 w_ic = np.linspace(0, 4., 6)
 w_rc = np.linspace(0, 0.9, 6)
-diameters = np.linspace(0., 15, 250)
-
+spatial_freqs = range(0, 90)
 #run simulator--------------------------------------------------------------------
-counter= 0
 
+counter= 0
+modify_wic(1.69)
 for wi, wr in zip(w_ic,w_rc):
     modify_wic(wi)
     modify_wrc(wr)
-    for d in diameters:
-        modify_diameter(d)
+    for Kd in spatial_freqs:
+        modify_spatial_freq(Kd)
 ##########################################################
         with open(config_file, 'w') as stream:
             yaml.dump(config_data, stream)
