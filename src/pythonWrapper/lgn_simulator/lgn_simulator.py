1b61bc4849e0e1b76fa70499cd92dcc711c172a5
Milad <miladh@ibv.uio.no>
2016-09-20 19:06:41 +0200

    updates

---
+++ src/pythonWrapper/lgn_simulator/lgn_simulator.py
@@ -0,0 +1,8 @@
+print "hello world!"
+def __bootstrap__():
+    global __bootstrap__, __loader__, __file__
+    import sys, pkg_resources, imp
+    __file__ = pkg_resources.resource_filename(__name__, 'lgn_simulator.so')
+    __loader__ = None; del __bootstrap__, __loader__
+    imp.load_dynamic(__name__,__file__)
+__bootstrap__()
