5297502db652c7778ab1c025a8f325e41437df08
Milad <miladh@ibv.uio.no>
2016-09-20 19:13:17 +0200

    added init.py

--- src/pythonWrapper/lgn_simulator/__init__.py
+++ src/pythonWrapper/lgn_simulator/__init__.py
@@ -2,7 +2,7 @@ print "hello world!"
 def __bootstrap__():
     global __bootstrap__, __loader__, __file__
     import sys, pkg_resources, imp
-    __file__ = pkg_resources.resource_filename(__name__, 'lgn_simulator.so')
+    __file__ = pkg_resources.resource_filename("lgn_simulator", 'lgn_simulator.so')
     __loader__ = None; del __bootstrap__, __loader__
-    imp.load_dynamic(__name__,__file__)
+    imp.load_dynamic("lgn_simulator",__file__)
 __bootstrap__()
