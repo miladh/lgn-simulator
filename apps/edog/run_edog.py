17aa83c982cf4a40fec455f9bbdd55b21cc0d2ef
Milad <miladh@ibv.uio.no>
2016-10-14 19:09:31 +0200

    summation curve with dog fb

--- apps/edog/run_edog.py
+++ apps/edog/run_edog.py
@@ -62,10 +62,10 @@ with open(config_file, 'r') as stream:
 
 
 #parameters---------------------------------------------------------------------
-#diameters = np.linspace(0., 15, 250)
+diameters = np.linspace(0., 15, 250)
 w_rc = np.linspace(0, 0.9, 4)
 w_rc_c = np.linspace(0, 3, 4)
-spatial_freqs = range(0, 90)
+
 
 #run simulator--------------------------------------------------------------------
 counter= 0
@@ -75,18 +75,18 @@ modify_wig(1.0)
 modify_arc(0.1)
 modify_brc(0.9)
 modify_crc(2.0)
-modify_diameter(6.69)
+
 
 for w in w_rc:
     modify_wrc(w)
-    # for Kd in spatial_freqs:
-    #     modify_spatial_freq(Kd)
+    for d in diameters:
+        modify_diameter(d)
 ##########################################################
-    with open(config_file, 'w') as stream:
-        yaml.dump(config_data, stream)
+        with open(config_file, 'w') as stream:
+            yaml.dump(config_data, stream)
 
-    run_id = '{0:04}'.format(counter)
-    st.run_simulator(config_file, record_label, run_id)
-    counter+=1
+        run_id = '{0:04}'.format(counter)
+        st.run_simulator(config_file, record_label, run_id)
+        counter+=1
 os.remove(config_file)
 ##########################################################
