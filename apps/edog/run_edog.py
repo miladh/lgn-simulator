5b6fcf6277e863cd8fa23d53eb7302f8a23f11e0
Milad <miladh@ibv.uio.no>
2016-10-15 14:13:18 +0200

    edog: image stim

--- apps/edog/run_edog.py
+++ apps/edog/run_edog.py
@@ -74,14 +74,12 @@ modify_diameter(8.01204819277)
 
 for w in weights:
     modify_wrc(w)
-    for Kd in spatial_freqs:
-        modify_spatial_freq(Kd)
 ##########################################################
-        with open(config_file, 'w') as stream:
-            yaml.dump(config_data, stream)
+    with open(config_file, 'w') as stream:
+        yaml.dump(config_data, stream)
 
-        run_id = '{0:04}'.format(counter)
-        st.run_simulator(config_file, record_label, run_id)
-        counter+=1
+    run_id = '{0:04}'.format(counter)
+    st.run_simulator(config_file, record_label, run_id)
+    counter+=1
 os.remove(config_file)
 ##########################################################
