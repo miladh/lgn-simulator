1f14356502c32c1d058ce0f1e891c9f41072be4c
Milad <miladh@ibv.uio.no>
2016-10-19 15:37:23 +0200

    edog: summation curve grating

--- apps/edog/run_edog.py
+++ apps/edog/run_edog.py
@@ -53,8 +53,8 @@ with open(config_file, 'r') as stream:
 
 
 #parameters---------------------------------------------------------------------
-# diameters = np.linspace(0., 15, 250)
-# weights = np.linspace(0, 0.9, 4)
+diameters = np.linspace(0., 15, 250)
+weights = np.linspace(0, 0.9, 4)
 # w_rc = np.linspace(0, 0.9, 4)
 # w_rc_c = np.linspace(0, 3, 4)
 # spatial_freqs = range(0, 90)
@@ -69,17 +69,17 @@ modify_brc(0.9)
 modify_crc(2.0)
 modify_wrig(-0.5)
 modify_arig(0.3)
-# modify_diameter(1.68674698795)
-# modify_diameter(8.01204819277)
 
-# for w in weights:
-#     modify_wrc(w)
+for w in weights:
+    modify_wrc(w)
+    for d in diameters:
+        modify_diameter(d)
 ##########################################################
-with open(config_file, 'w') as stream:
-    yaml.dump(config_data, stream)
+        with open(config_file, 'w') as stream:
+            yaml.dump(config_data, stream)
 
-run_id = '{0:04}'.format(counter)
-st.run_simulator(config_file, record_label, run_id)
-counter+=1
+        run_id = '{0:04}'.format(counter)
+        st.run_simulator(config_file, record_label, run_id)
+        counter+=1
 os.remove(config_file)
 ##########################################################
