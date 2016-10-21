6ed65a917acd3d9ea3684407715075174fa5bb8d
Milad <miladh@ibv.uio.no>
2016-10-21 15:38:52 +0200

    convergence ds

--- apps/edog/run_edog.py
+++ apps/edog/run_edog.py
@@ -63,7 +63,7 @@ with open(config_file, 'r') as stream:
 #parameters---------------------------------------------------------------------
 # diameters = np.linspace(0., 15, 250)
 # weights = np.linspace(0, 0.6, 3)
-# w_rc = np.linspace(0, 0.9, 4)
+w_rc = np.linspace(0, 0.9, 4)
 # w_rc_c = np.linspace(0, 3, 4)
 # spatial_freqs = range(0, 90)
 # widths = np.linspace(0, 3, 30)
@@ -88,12 +88,14 @@ modify_spatial_freq(4)
 
 for ds in ds_vec:
     modify_spatial_resolution(ds)
+    for w in w_rc:
+        modify_wrc(w)
 ##########################################################
-    with open(config_file, 'w') as stream:
-        yaml.dump(config_data, stream)
+        with open(config_file, 'w') as stream:
+            yaml.dump(config_data, stream)
 
-    run_id = '{0:04}'.format(counter)
-    st.run_simulator(config_file, record_label, run_id)
-    counter+=1
+        run_id = '{0:04}'.format(counter)
+        st.run_simulator(config_file, record_label, run_id)
+        counter+=1
 os.remove(config_file)
 ##########################################################
