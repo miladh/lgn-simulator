289a9a9ab95366dd9be9d3ceea7641b016d8e342
Milad <miladh@ibv.uio.no>
2016-10-21 13:25:45 +0200

    convergence ds

--- apps/edog/run_edog.py
+++ apps/edog/run_edog.py
@@ -10,6 +10,13 @@ sys.path.append(os.path.abspath(os.path.join(current_path,"../../tools")))
 import sumatra_tracking.run_simulator as st
 
 
+def modify_num_spatial_points(ns):
+    config_data["grid"]["ns"] = int(ns)
+
+def modify_spatial_resolution(ds):
+    config_data["grid"]["ds"] = float(ds)
+
+
 def modify_phase(phase):
     config_data["stimulus"]["phase"] = float(phase)
 
@@ -55,16 +62,19 @@ with open(config_file, 'r') as stream:
 
 #parameters---------------------------------------------------------------------
 # diameters = np.linspace(0., 15, 250)
-weights = np.linspace(0, 0.6, 3)
+# weights = np.linspace(0, 0.6, 3)
 # w_rc = np.linspace(0, 0.9, 4)
 # w_rc_c = np.linspace(0, 3, 4)
-spatial_freqs = range(0, 90)
+# spatial_freqs = range(0, 90)
 # widths = np.linspace(0, 3, 30)
+
+ds_vec = linspace(0.01, 0.2, 20)
 
 
 #run simulator--------------------------------------------------------------------
 counter= 0
 
+modify_wrc(0.5)
 modify_arc(0.1)
 modify_brc(0.9)
 modify_crc(2.0)
@@ -72,17 +82,18 @@ modify_wrig(-0.5)
 modify_arig(0.3)
 # modify_diameter(1.68674698795)
 modify_diameter(10.)
+modify_spatial_freq(4)
 
-for w in weights:
-    modify_wrc(w)
-    for Kd in spatial_freqs:
-        modify_spatial_freq(Kd)
+
+
+for ds in ds_vec:
+    modify_spatial_resolution(ds)
 ##########################################################
-        with open(config_file, 'w') as stream:
-            yaml.dump(config_data, stream)
+    with open(config_file, 'w') as stream:
+        yaml.dump(config_data, stream)
 
-        run_id = '{0:04}'.format(counter)
-        st.run_simulator(config_file, record_label, run_id)
-        counter+=1
+    run_id = '{0:04}'.format(counter)
+    st.run_simulator(config_file, record_label, run_id)
+    counter+=1
 os.remove(config_file)
 ##########################################################
