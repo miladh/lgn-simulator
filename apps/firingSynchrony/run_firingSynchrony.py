7daac8516272e9b81039834cc5483db0ca217db3
Milad <miladh@ibv.uio.no>
2016-10-31 14:44:03 +0100

    temp freq run

--- apps/firingSynchrony/run_firingSynchrony.py
+++ apps/firingSynchrony/run_firingSynchrony.py
@@ -30,6 +30,9 @@ def modify_diameter(d):
     config_data["stimulus"]["maskSize"] = float(d)
 
 
+def modify_surround_temp_freq(wId):
+    config_data["stimulus"]["surroundwId"] = float(wId)
+
 
 def modify_wrc(w):
     config_data["relay"]["Krc"]["w"] = float(w)
@@ -75,6 +78,8 @@ weights = np.linspace(0, 0.9, 4)
 # weights_c = [0.5, 1.5, 2.0]
 # widths = [0.1, 0.5, 2.5]
 # widths_b = [0.9, 2.5, 0.5]
+wId_vec = range(-5, 6)
+
 
 #run simulator--------------------------------------------------------------------
 counter= 0
@@ -86,14 +91,17 @@ modify_wrig(-0.5)
 modify_arig(0.3)
 
 
+
 for w in weights:
     modify_wrc(w)
+    for wId in wId_vec:
+        modify_surround_temp_freq(wId)
 ##########################################################
-    with open(config_file, 'w') as stream:
-        yaml.dump(config_data, stream)
+        with open(config_file, 'w') as stream:
+            yaml.dump(config_data, stream)
 
-    run_id = '{0:04}'.format(counter)
-    st.run_simulator(config_file, record_label, run_id)
-    counter+=1
+        run_id = '{0:04}'.format(counter)
+        st.run_simulator(config_file, record_label, run_id)
+        counter+=1
 os.remove(config_file)
 ##########################################################
